fafb13939bc23932a68cd4b85ac5e5a0220aa002
Agustin Kassis <webmaster@masize.com>
2022-09-10 04:10:02 -0300

    Update FaucetBlock.jsx

--- src/components/FaucetBlock.jsx
+++ src/components/FaucetBlock.jsx
@@ -2,9 +2,6 @@ import React from "react";
 import { Button } from "../components/Button";
 import { AddToken } from "./AddToken";
 
-import usdcImage from "../static/usdc.png";
-import peImage from "../static/pe.png";
-
 // async function handleClaimPE(address) {
 //   console.info("address", address);
 
@@ -38,14 +35,14 @@ export const FaucetBlock = ({ address }) => {
         address={"0xc2768beF7a6BB57F0FfA169a9ED4017c09696FF1"}
         symbol={"PE"}
         decimals={6}
-        image={peImage}
+        image="https://raw.githubusercontent.com/peronio-ar/branding/main/logo/v2/200.png"
       />
       <br />
       <AddToken
         address={"0x2791bca1f2de4661ed88a30c99a7a9449aa84174"}
         symbol={"USDC"}
         decimals={6}
-        image={usdcImage}
+        image="https://polygonscan.com/token/images/centre-usdc_32.png"
       />
       <br />
       <Button
